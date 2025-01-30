32944efbfa15698ef8b1ad22c3fcef3bb060c2a7
smd255 <sumida.shogo368@gmail.com>
2025-01-30 22:42:06 +0900

    refactor

--- src/fastapi_memoapp/main.py
+++ src/fastapi_memoapp/main.py
@@ -1,55 +1,36 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from pydantic import ValidationError
-from schemas.memo import InsertAndUpdateMemoSchema, MemoSchema, ResponseSchema
+from routers.memo import router as memo_router
 
 #===========================================
 # 起動ファイル
 #===========================================
 app = FastAPI()
 
-#===========================================
-# メモ用のエンドポイント
-#===========================================
-# メモ新規登録
-# 新規登録時は
-@app.post("/memos", response_model=ResponseSchema)
-async def create_memo(memo: InsertAndUpdateMemoSchema):
-    print(memo)
-    return ResponseSchema(message="メモが正常に登録されました")
+# CORS設定
+app.add_middleware(
+    CORSMiddleware,
+    # 許可するオリジン(URL)を指定
+    allow_origins=["http://127.0.0.1:5500"],
+    # 認証情報を含むリクエストを許可
+    allow_credentials=True,
+    # 許可するHTTPメソッドを指定
+    allow_methods=["*"],
+    # 許可するHTTPヘッダーを指定
+    allow_headers=["*"],
+)
 
-# メモ情報全件取得
-@app.get("/memos", response_model=list[MemoSchema])
-async def get_memo_list():
-    return [
-        MemoSchema(title="タイトル1", description="詳細1", memo_id=1),
-        MemoSchema(title="タイトル2", description="詳細2", memo_id=2),
-        MemoSchema(title="タイトル3", description="詳細3", memo_id=3)
-    ]
-
-# 特定のメモ情報取得
-@app.get("/memos/{memo_id}", response_model=MemoSchema)
-async def get_memo_detail(memo_id: int):
-    return MemoSchema(title="タイトル1", description="詳細1", memo_id=memo_id)
-
-# 特定のメモを更新
-@app.put("/memos/{memo_id}", response_model=ResponseSchema)
-async def modify_memo(memo_id: int, memo: InsertAndUpdateMemoSchema):
-    print(memo_id, memo)
-    return ResponseSchema(message="メモが正常に更新されました")
-
-# 特定のメモを削除
-@app.delete("/memos/{memo_id}",response_model=ResponseSchema)
-async def remove_memo(memo_id: int):
-    print(memo_id)
-    return ResponseSchema(message="メモが正常に削除されました")
+# ルーターのマウント
+app.include_router(memo_router)
 
 # バリデーションエラーのカスタムハンドラ
 @app.exception_handler(ValidationError)
 async def validation_exception_handler(exc: ValidationError):
-    # ValidationErrorが発生した場合にクライアントに返すレスポンス定義
+    # ValidationErrorが発生した場合にクライアントに必ず返すレスポンス定義
     return JSONResponse(
-        # ステータスコード422 Unprocessable Entity
+        # ステータスコード422
         status_code=422,
         # エラーの詳細
         content={
